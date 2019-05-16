e7decaa8467bed215dd9d131f45800aea3dcbcaf
Darius Seals <tristarmediagroupinc@gmail.com>
2019-05-15 20:25:39 -0400

    init commit

---
+++ src/Hangman.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { randomWord } from './words.js'
+import "./Hangman.css";
+import img0 from "./0.jpg";
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+import img5 from "./5.jpg";
+import img6 from "./6.jpg";
+
+class Hangman extends Component {
+  static defaultProps = {
+    maxWrong: 6,
+    images: [img0, img1, img2, img3, img4, img5, img6]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord()};
+    this.handleGuess = this.handleGuess.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  reset(){
+    this.setState({nWrong: 0, guessed: new Set(), answer: randomWord()});
+  }
+
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
+
+
+  handleGuess(evt) {
+    let ltr = evt.target.value;
+    this.setState(st => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+    }));
+  }
+
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
+      <button
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+        key={ltr}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+
+  render() {
+    const gameIsOver = this.state.nWrong >= this.props.maxWrong;
+    const altText = `${this.state.nWrong}/ ${this.props.maxWrong} gueses`;
+    const isWinner = this.guessedWord().join("") === this.state.answer;
+    let gameState = this.generateButtons();
+
+    if(gameIsOver) gameState = "Sorry, You Lose!" ;
+    if(isWinner) gameState = "Congrats!, You WIN!" ;
+
+    return (
+      <div className='Hangman'>
+        <h1>Hangman</h1>
+        <img src={this.props.images[this.state.nWrong]} alt={altText}/>
+        <p>Number of wrong guesses: {this.state.nWrong}</p>
+        <p className='Hangman-word'>
+          {!gameIsOver ? this.guessedWord() : this.state.answer}
+        </p>
+        <p className='Hangman-btns'>
+          {gameState}
+        </p>
+        <button id="reset" onClick={this.reset}>Restart ?</button>
+      </div>
+    );
+  }
+}
+
+export default Hangman;
